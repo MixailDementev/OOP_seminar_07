82dc9ce61f73a0a889882e140634d7b234957709
MDementev <vanneste1207@gmail.com>
2023-02-07 21:43:11 +0300

    DZ6

---
+++ Human.java
@@ -0,0 +1,18 @@
+
+public class Human {
+    String Name;
+    String Sex;
+    int Age;
+
+    public Human(String name, String sex, int age){
+        this.Name=name;
+        this.Sex=sex;
+        this.Age=age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+
+}
